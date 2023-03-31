dd0548555eb0ffeadde7b459c594060ed1720ac5
jeris90 <jeris62@gmail.com>
2023-03-31 12:09:40 +0200

    CNF Parser + task classes

--- src/appx/Launcher.java
+++ src/appx/Launcher.java
@@ -1,12 +1,21 @@
 package appx;
 
+import java.util.LinkedList;
+
+import appx.af.ArgumentationFramework;
+import appx.parser.AFParser;
+
 public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("test2");
-		System.out.println("test3");
+		ArgumentationFramework af = AFParser.readingCNF("/Users/jdelobelle/Documents/recherche/approximation_semantique/instances/BA-numArg10-pbCycle0-0.cnf");
+		
+		/*af.add_attack(0, 1);
+		af.add_attack(1, 0);
+		af.add_attack(1, 2);
+		*/
+		System.out.println(af);
 	}
 
 }
---
+++ src/appx/af/ArgumentationFramework.java
@@ -0,0 +1,43 @@
+package appx.af;
+
+import java.util.LinkedList;
+
+public class ArgumentationFramework {
+	
+	private LinkedList<Integer> [] af_attacker;
+	
+	private LinkedList<Integer> [] af_attackee;
+	
+	public ArgumentationFramework(int nb_arg) {
+		af_attacker = new LinkedList[nb_arg];
+		af_attackee = new LinkedList[nb_arg];
+		
+		for (int i=0 ; i < nb_arg ; i++) {
+			af_attacker[i] = new LinkedList<Integer>(); 
+			af_attackee[i] = new LinkedList<Integer>(); 
+		}
+	}
+	
+	public void add_attack(int attacker, int target) {
+		af_attacker[target].add(attacker);
+		af_attackee[attacker].add(target);
+	}
+	
+	public LinkedList<Integer>[] get_af_attacker(){
+		return af_attacker;
+	}
+	
+	public LinkedList<Integer>[] get_af_attackee(){
+		return af_attackee;
+	}
+	
+	public String toString() {
+		StringBuilder res = new StringBuilder();
+		int arg = -1;
+		for (int i=0 ; i < af_attacker.length ; i++) {
+			res.append(i + " -> " + af_attacker[i].toString() + "\n");
+		}
+		return res.toString();
+	}
+
+}
